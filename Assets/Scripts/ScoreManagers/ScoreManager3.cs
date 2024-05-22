b3c70243f68db86f4968dde0198f7a56358f5e39
mentos120 <86073726+mentos120@users.noreply.github.com>
2024-05-22 20:49:24 +0200

    Level3
    UZ NEVIM CO SEMKA PSAT ALE JE TO PAIN

--- Assets/Scripts/ScoreManagers/ScoreManager3.cs
+++ Assets/Scripts/ScoreManagers/ScoreManager3.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,27 +10,49 @@ public class ScoreManager3 : MonoBehaviour
 	private GameObject[] enemies;
 	[SerializeField] private int mags;
 	[SerializeField] private GameObject escapeWall;
+	[SerializeField] private GameObject[] walls;
 	[SerializeField] private GameObject text;
+	private float delay = 0.4f;
 	// Start is called before the first frame update
 	void Start()
-    {
+	{
 		player = GameObject.Find("Player");
 		player.GetComponent<PlayerController>().setMagAmount(mags);
 		enemies = GameObject.FindGameObjectsWithTag("Enemy");
 	}
 
-    // Update is called once per frame
-    void Update()
-    {
-		if (enemies.Length == 0)
+	// Update is called once per frame
+	void Update()
+	{
+		if (delay <= 0)
 		{
-			escapeWall.SetActive(false);
-			text.SetActive(true);
+			switch (enemies.Length)
+			{
+				case 0:
+					escapeWall.SetActive(false);
+					text.SetActive(true);
+					break;
+				case 60:
+					walls[0].SetActive(false);
+					break;
+				case 54:
+					walls[1].SetActive(false);
+					break;
+				case 42:
+					walls[2].SetActive(false);
+					break;
+				case 24:
+					walls[3].SetActive(false);
+					break;
+			}
 		}
 		else
 		{
-			enemies = GameObject.FindGameObjectsWithTag("Enemy");
+			delay -= Time.deltaTime;
 		}
+		
+		enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		Debug.Log(enemies.Length);
 	}
 	public void back()
 	{
